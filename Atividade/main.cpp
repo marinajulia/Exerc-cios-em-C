59b12a28c46de9fd14136a46627af65691600bd2
marinajulia <marinajulia707@gmail.com>
2021-08-27 15:57:13 -0300

    atividade

---
+++ Atividade/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void inserir(int a[]){
+	int i;
+	for(i = 0; i < 3; i++){
+		printf("Digite um valor %d: 	", i);
+		scanf("%d", &a[i]);
+	}
+}
+
+void imprimir(int b[]){
+	int i;
+		for(i = 0; i < 3; i++){
+		printf("\n Números[%d] = %d", i, (2*b[i]));
+	}
+	
+}
+int main(int argc, char** argv) {
+	int numero[3];
+	printf("\n Preenchendo o vetor: \n");
+	inserir(numero);
+	
+	printf("\n valor final");
+	imprimir(numero);
+	return 0;
+}
